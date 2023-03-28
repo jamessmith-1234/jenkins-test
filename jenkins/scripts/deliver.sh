7a82a8202e70fe2084ed32e5972bc2c40e62a719
jamessmith-1234 <prettyduck11@gmail.com>
2023-03-28 16:54:37 -0400

    dfkljasdfjkl

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,7 +6,8 @@ echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
+# set +x
+set a+rx
 
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
